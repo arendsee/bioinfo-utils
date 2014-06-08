e0e04750b3d2c07bb711d1d777d88c982a6947cc
Zebulun Arendsee <arendsee@iastate.edu>
2014-06-08 18:56:19 -0500

    initial commit

---
+++ phytable.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# INPUT
+#  list of filenames (<Genus>_<species>*)
+#  read from STDIN
+# OUTPUT
+#  white space delimited file with columns: (taxid, sciname, lineage)
+#  written to STDOUT
+# USAGE
+#  ls <genome files> | phytable > phyinfo.txt
+# NOTE
+#  Errors are silent. If the species associated with a file cannot be found,
+#  the program will not complain. You will have to check yourself.
+# REQUIRES
+#  starlet
+
+# Get scientific names for each genome from the filenames
+scinames=''
+while read line; do
+    for f in $line; do
+        scinames="$scinames "`basename $f | \
+            grep -E '[A-Z][a-z]+_[a-z]+' | \
+            perl -pe 's/^([A-Z][a-z]+_[a-z]+).*/\1/' | \
+            tr '_' '+'`
+    done
+done
+
+scinames=`echo $scinames | tr ' ' '\n' | sort -u | tr '\n' ' '`
+
+# Retrieve the taxon ids for each species from entrez 
+ENTREZ='http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+
+taxids=''
+echo "Retrieving lineages from entrez" > /dev/stderr
+for sciname in $scinames; do
+    echo -n '.' > /dev/stderr
+    taxids="$taxids "`wget -O - \
+        "$ENTREZ/esearch.fcgi?db=taxonomy&term=$sciname" 2> /dev/null | \
+        xmlstarlet sel -t -m '/eSearchResult/IdList' -v Id -o ','`
+    sleep .3s # To avoid getting my IP blocked by NCBI
+done
+
+# Remove terminal comma, remove blanks
+taxids=`echo $taxids | perl -pe 's/,$//' | perl -pe 's/, ,/,/'`
+
+# Retrieve the phylogenetic lineage of each taxid from entrez
+wget -O - "$ENTREZ/efetch.fcgi?db=taxonomy&id=$taxids"  2> /dev/null | \
+    xmlstarlet sel \
+        -t -m 'TaxaSet/Taxon' \
+        -v TaxId -o '|' \
+        -v ScientificName -o ',' \
+            -m 'OtherNames/Synonym' \
+            -v "text()" -o ',' -b \
+        -o '|' \
+        -v Lineage -n | \
+        perl -pe 's/;\s/;/g' | tr ' ' '_' | tr '|' ' ' | perl -pe 's/, / /g' 
+
+echo " Done" > /dev/stderr
