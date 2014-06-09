ab245511899fba91fd6d493c70e4670329291410
Zebulun Arendsee <arendsee@iastate.edu>
2014-06-08 20:11:32 -0500

    added argument input to sciname2taxid (previously was stdin only)

--- sciname2taxid.sh
+++ sciname2taxid.sh
@@ -3,9 +3,19 @@
 # Filter to convert scientific names to taxon ids
 # One name per line (spaces or underscores to separate words)
 
+scinames=''
+if [[ $# -gt 0 ]]; then
+    scinames="$@" 
+else
+    while read line; do
+        line=`echo $line | tr ' ' '_'`
+        scinames="$scinames $line"
+    done
+fi
+
 base='http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-while read line; do
-    sciname=`echo $line | tr '_' '+'`
+for sciname in $scinames; do
+    sciname=`echo $sciname | tr '_' '+'`
     wget -O - "$base/esearch.fcgi?db=taxonomy&term=$sciname" 2> /dev/null |
         xmlstarlet sel -t -v '/eSearchResult/IdList/Id'
     sleep 0.3s # To prevent NCBI from complaining
