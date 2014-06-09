52ecddc0f2344655d55bc422220bd9ba14ffd6e8
Zebulun Arendsee <arendsee@iastate.edu>
2014-06-08 21:37:08 -0500

    added fasta2blast_taxidmap.sh

---
+++ fasta2blast_taxidmap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# INPUT - file names with species name preceding first dot, e.g. Zea_mays.mito.faa
+# OUTPUT - a taxid map with following format
+
+filenames=''
+if [[ $# -gt 0 ]]; then
+    filenames="$@" 
+else
+    while read line; do
+        filenames="$filenames $line"
+    done
+fi
+
+base='http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+for f in $filenames; do
+    sciname=`basename "$f" | perl -pe 's/([^.]+).*/$1/' | tr '_' '+'`
+    taxid=`wget -O - "$base/esearch.fcgi?db=taxonomy&term=$sciname" 2> /dev/null |
+        xmlstarlet sel -t -v '/eSearchResult/IdList/Id'`
+    sleep 0.3s # To prevent NCBI from complaining
+    while read header; do
+        echo "$header $taxid"
+    done < <(grep '>' $f) |
+        # This may be overly simplistic. It seems that ncbi-blast just grabs
+        # the first "word", but I may be wrong.
+        perl -pe 's/^>(\S+).*?(\d+)$/$1 $2/'
+done
