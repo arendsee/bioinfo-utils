45f39c746131bb1e7ac601b15172cead3def3ecd
Zebulun Arendsee <arendsee@iastate.edu>
2014-10-20 22:30:38 -0500

    fixed tozero.pl

--- tozero.pl
+++ tozero.pl
@@ -1,13 +1,21 @@
 #!/usr/bin/env perl
 
+# Examples
+# ./tozero.pl <<< "asdf0.0001asdas1.09f"
+# ./tozero.pl 0.001 <<< "asdf0.0001asdas1.09f"
+# ./tozero.pl 0.001 asdf <<< "asdf0.0001asdas1.09f"
+# ./tozero.pl 5 '' f <<< "asdf0.0001asdas1.09f"
+
 my $min = shift;
-$min = $min ? $min : 8;
+$min = $min ? $min : 0.001;
 my $for = shift;
 $for = $for ? $for : '';
 my $aft = shift;
 $aft = $aft ? $aft : '';
 
+print "$min|$for|$aft|\n";
+
 while(<>){
-    $_ =~ s/$for(\d\.?\d+)$aft/$for . $1 < $min ? 0 : $1 . $aft/ge; 
+    $_ =~ s/($for)(\d\.?\d+)($aft)/$1 . ($2 < $min ? 0 : $3) . $aft/ge; 
     print $_, "\n";
 }
