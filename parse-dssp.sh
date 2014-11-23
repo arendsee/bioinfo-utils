70564a73a92a79fba83db1114db8b1a690dcd493
Zebulun Arendsee <arendsee@iastate.edu>
2014-11-23 17:23:33 -0600

    added dssp parser

---
+++ parse-dssp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Input: a PDB file
+# Output: TAB-delimited line containing secondary structure counts
+#   1. structure id - taken from the PDB filename
+#   2. beta-strand count
+#   3. alpha-helix count
+#   4. total residue count
+# Recommended usage:
+# ls *.pdb | parallel parse-dssp.sh {} > ss.tab
+
+dssp -i $1 |
+    sed -n '/#/,$ p' |
+    sed '1d' |
+    cut -b17 |
+    sort | uniq -c |
+    awk -v name=$(basename $j | sed 's/\.pdb//') '
+        BEGIN{
+            OFS="\t"
+            E = 0
+            H = 0
+            N = 0
+        }
+        {N += $1}
+        $2 ~ /[BE]/ {E += $1}
+        $2 ~ /[GHI]/ {H += $1}
+        END{print name, E, H, N}'
