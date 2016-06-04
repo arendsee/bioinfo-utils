ab9d247cffc9a611cb4b8c36657db860d52d1870
Zebulun Arendsee <arendsee@iastate.edu>
2016-06-04 02:24:46 -0500

    Add rentrez info script

---
+++ rentrez-tools/reu-info.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("argparse"))
+
+parser <- ArgumentParser(
+  description='Get info on entrez databases',
+  usage='reu-info.R [options]')
+
+parser$add_argument(
+  '-v', '--version',
+  action='store_true',
+  default=FALSE)
+
+suppressPackageStartupMessages(library("rentrez"))
+
+parser$add_argument(
+  '-s', '--summary',
+  help='Print summary for given database'
+)
+
+parser$add_argument(
+  '-x', '--searchable',
+  help='Print the set of search terms that can be used for the given database'
+)
+
+parser$add_argument(
+  '-l', '--links',
+  help='Print databases that may contain links to this database'
+)
+
+args <- parser$parse_args()
+
+if(args$version){
+  cat(sprintf('rstab v%s\n', version))
+  q()
+}
+
+alldb <- entrez_dbs()
+
+if(length(args$summary) != 0){
+  entrez_db_summary(args$summary)
+} else if(length(args$searchable) != 0){
+  entrez_db_searchable(args$searchable)
+} else if(length(args$links) != 0){
+  entrez_db_links(args$links)
+} else {
+  write.table(alldb, row.names=FALSE, quote=FALSE)
+}
