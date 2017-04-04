7f007d563c3f7b819391996a44f58adb42541a84
Zebulun Arendsee <arendsee@iastate.edu>
2017-04-04 14:23:42 -0500

    Add script for fast slurming

---
+++ autoslurm.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# DESCRIPTION:
+#   Wraps the given code in a slurm file and runs it
+#   The default walltime is 2 hours 
+#   Any number of slurm options may follow the script
+# USAGE: ./autoslurm.sh mycode.sh [options]
+
+code=$1
+shift
+
+base=${code%.*}
+filename="__${base}.pbs"
+
+[[ -d archive ]] || mkdir archive
+mv -f err* out* archive
+if [[ -f $filename ]]
+then
+    mv $filename archive/$filename.$RANDOM
+fi
+
+cat > $filename << EOL
+#!/usr/bin/env bash
+#SBATCH --job-name=$base
+#SBATCH --error=err_$base.%J
+#SBATCH --output=out_$base.%J
+EOL
+
+[[ `echo "$@" | grep -- '--time'` ]] || echo '#SBATCH --time=2:00:00' >> $filename
+
+for o in $@
+do
+    echo "#SBATCH $o"
+done >> $filename
+
+echo -e "\ncd \$SLURM_SUBMIT_DIR" >> $filename
+
+cat $code |
+    # remove the hasbang line
+    grep -v '^#!' >> $filename
+
+# submit the script
+sbatch $filename && echo "Job Submitted" >&2 || echo "Failed to submit job" >&2
